4c27b69b776c39718b975b02fb03b21ac0ca018b
MeMeee1 <82448794+MeMeee1@users.noreply.github.com>
2023-12-20 23:43:03 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//custom message that shows the user he/she should input their birthdate
+var ageInput = document.getElementById("ageInput");
+var nameInput = document.getElementById("nameInput");
+
+var emailInput= document.getElementById("emailInput");
+
+var phoneInput= document.getElementById("phoneInput");
+
+var subjectInput= document.getElementById("subjectInput");
+
+var messageInput= document.getElementById("messageInput");
+function ShowContactDetails()
+{
+  window.alert("Nice to meet you " +nameInput.value +". Here is what you sent us.\n"+ "Name: "+nameInput.value+"\n"+"Email: "+emailInput.value+"\n" +  "Subject: "+subjectInput.value+"\n"+"Message: "+messageInput.value);
+}
+///
+function ShowDateField() {
+  ageInput.type = 'date';
+}
+function HideDateField() {
+  ageInput.type = 'text';
+}
+function OnHideInfo() {
+  document.getElementById("modal01").style.display = "none";
+}
+function onClick(element) {
+  window.alert("Submitted Successfully")
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.querySelector("#mySidebar");
+function open_menu() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    console.log("this stuff works");
+  } else {
+    mySidebar.style.display = 'block';
+    console.log("this stuff works");
+  }
+}
+
+// Close the sidebar with the close button
+function close_menu() {
+  mySidebar.style.display = "none";
+  console.log("this stuff close");
+}
